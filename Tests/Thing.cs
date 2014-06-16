23109d4a64c7af3a2fae9fc3efa8afe352b734c0
Nick Strupat <nick.strupat@gmail.com>
2014-06-16 15:34:44 -0400

    Fixed clean pull build

---
+++ Tests/Thing.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests {
+    public class Thing {
+        public Int64 Id { get; protected set; }
+        public String Value { get; set; }
+    }
+}
