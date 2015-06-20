873513c68a648b1dc28c5349d5aa3f95a929e18d
Nick Strupat <nick.strupat@gmail.com>
2015-06-20 00:03:31 -0400

    update version

--- EntityFramework.Triggers/Properties/AssemblyInfo.cs
+++ EntityFramework.Triggers/Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("5.0.4.248")]
-[assembly: AssemblyFileVersion("5.0.4.248")]
+[assembly: AssemblyVersion("5.1.0.289")]
+[assembly: AssemblyFileVersion("5.1.0.289")]
