c51cdb0ec245ea810cf8d6ca38bbe19a470a9aa1
Nick Strupat <nick.strupat@gmail.com>
2015-07-21 23:28:29 -0400

    "forward" the parameterized constructors from DbContext to our DbContextWithTriggers

--- EntityFramework.Triggers/DbContextWithTriggers.cs
+++ EntityFramework.Triggers/DbContextWithTriggers.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.Data.Common;
 using System.Data.Entity;
+using System.Data.Entity.Core.Objects;
+using System.Data.Entity.Infrastructure;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -16,5 +19,92 @@ namespace EntityFramework.Triggers {
 			return this.SaveChangesWithTriggersAsync(cancellationToken);
 		}
 #endif
-	}
+
+        //
+        // Summary:
+        //     Constructs a new context instance using conventions to create the name of the
+        //     database to which a connection will be made. The by-convention name is the full
+        //     name (namespace + class name) of the derived context class. See the class remarks
+        //     for how this is used to create a connection.
+        protected DbContextWithTriggers() : base() { }
+        //
+        // Summary:
+        //     Constructs a new context instance using conventions to create the name of the
+        //     database to which a connection will be made, and initializes it from the given
+        //     model. The by-convention name is the full name (namespace + class name) of the
+        //     derived context class. See the class remarks for how this is used to create a
+        //     connection.
+        //
+        // Parameters:
+        //   model:
+        //     The model that will back this context.
+        protected DbContextWithTriggers(DbCompiledModel model) : base(model) { }
+        //
+        // Summary:
+        //     Constructs a new context instance using the given string as the name or connection
+        //     string for the database to which a connection will be made. See the class remarks
+        //     for how this is used to create a connection.
+        //
+        // Parameters:
+        //   nameOrConnectionString:
+        //     Either the database name or a connection string.
+        public DbContextWithTriggers(String nameOrConnectionString) : base(nameOrConnectionString) { }
+        //
+        // Summary:
+        //     Constructs a new context instance using the existing connection to connect to
+        //     a database. The connection will not be disposed when the context is disposed
+        //     if contextOwnsConnection is false.
+        //
+        // Parameters:
+        //   existingConnection:
+        //     An existing connection to use for the new context.
+        //
+        //   contextOwnsConnection:
+        //     If set to true the connection is disposed when the context is disposed, otherwise
+        //     the caller must dispose the connection.
+        public DbContextWithTriggers(DbConnection existingConnection, Boolean contextOwnsConnection) : base(existingConnection, contextOwnsConnection) { }
+        //
+        // Summary:
+        //     Constructs a new context instance around an existing ObjectContext.
+        //
+        // Parameters:
+        //   objectContext:
+        //     An existing ObjectContext to wrap with the new context.
+        //
+        //   dbContextOwnsObjectContext:
+        //     If set to true the ObjectContext is disposed when the DbContext is disposed,
+        //     otherwise the caller must dispose the connection.
+        public DbContextWithTriggers(ObjectContext objectContext, bool dbContextOwnsObjectContext) : base(objectContext, dbContextOwnsObjectContext) { }
+        //
+        // Summary:
+        //     Constructs a new context instance using the given string as the name or connection
+        //     string for the database to which a connection will be made, and initializes it
+        //     from the given model. See the class remarks for how this is used to create a
+        //     connection.
+        //
+        // Parameters:
+        //   nameOrConnectionString:
+        //     Either the database name or a connection string.
+        //
+        //   model:
+        //     The model that will back this context.
+        public DbContextWithTriggers(String nameOrConnectionString, DbCompiledModel model) : base(nameOrConnectionString, model) { }
+        //
+        // Summary:
+        //     Constructs a new context instance using the existing connection to connect to
+        //     a database, and initializes it from the given model. The connection will not
+        //     be disposed when the context is disposed if contextOwnsConnection is false.
+        //
+        // Parameters:
+        //   existingConnection:
+        //     An existing connection to use for the new context.
+        //
+        //   model:
+        //     The model that will back this context.
+        //
+        //   contextOwnsConnection:
+        //     If set to true the connection is disposed when the context is disposed, otherwise
+        //     the caller must dispose the connection.
+        public DbContextWithTriggers(DbConnection existingConnection, DbCompiledModel model, Boolean contextOwnsConnection) : base(existingConnection, model, contextOwnsConnection) { }
+    }
 }
