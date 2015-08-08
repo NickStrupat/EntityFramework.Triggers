18874fcbcde4c08167864ac65b95c3570434cec4
Nick Strupat <nick.strupat@gmail.com>
2015-08-08 02:31:51 -0400

    update some assembly info

--- EntityFramework.Triggers/Properties/AssemblyInfo.cs
+++ EntityFramework.Triggers/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("EntityFramework.Triggers")]
-[assembly: AssemblyDescription("Triggers for entity framework")]
+[assembly: AssemblyDescription("Triggers for Entity Framework")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
