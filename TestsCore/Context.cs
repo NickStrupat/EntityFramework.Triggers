485ba5b546b3f69c8d32c066ca2c6fe53a008c4e
Nick Strupat <nick.strupat@gmail.com>
2016-11-23 00:47:04 -0500

    private setter for EF Core works; need to do the initial migration for a new DB

--- TestsCore/Context.cs
+++ TestsCore/Context.cs
@@ -13,30 +13,10 @@ using System.Data.Entity.Migrations;
 namespace EntityFramework.Triggers.Tests {
 #endif
 
-	public class InternalContext : DbContext {
-		//        public override Int32 SaveChanges() {
-		//            return base.SaveChanges();
-		//        }
-		//#if !NET40
-		//        public override Task<Int32> SaveChangesAsync(CancellationToken cancellationToken) {
-		//            return base.SaveChangesAsync(cancellationToken);
-		//        }
-		//#endif
-	}
-
-	//internal sealed class Configuration : DbMigrationsConfiguration<Context> {
-	//	public Configuration() {
-	//		AutomaticMigrationsEnabled = true;
-	//		AutomaticMigrationDataLossAllowed = true;
-	//	}
-	//}
-
-	public class Context : DbContext {
+	public class Context : DbContextWithTriggers {
 #if EF_CORE
 		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {
-			//optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=EntityFrameworkCore.Triggers.Tests;Trusted_Connection=True;");
-			//optionsBuilder.UseSqlServer(@"Data Source=(localdb)\MSSQLLocalDB;Integrated Security=True;Connect Timeout=15;Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
-			optionsBuilder.UseSqlServer(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=EntityFramework.Triggers.Tests.Context;Integrated Security=True;Connect Timeout=15;Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+			optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=EntityFrameworkCore.Triggers.Tests;Trusted_Connection=True;");
 		}
 #endif
 
@@ -44,14 +24,5 @@ namespace EntityFramework.Triggers.Tests {
 		public DbSet<Thing>     Things     { get; set; }
 		public DbSet<Apple>     Apples     { get; set; }
 		public DbSet<RoyalGala> RoyalGalas { get; set; }
-
-		public override Int32 SaveChanges() {
-			return this.SaveChangesWithTriggers();
-		}
-#if !NET40
-		public override Task<Int32> SaveChangesAsync(CancellationToken cancellationToken = default(CancellationToken)) {
-			return this.SaveChangesWithTriggersAsync(cancellationToken);
-		}
-#endif
 	}
 }
--- TestsCore/Entity.cs
+++ TestsCore/Entity.cs
@@ -7,13 +7,8 @@ namespace EntityFramework.Triggers.Tests {
 #endif
 
 	public abstract class EntityWithInsertTracking {
-#if EF_CORE
-		public DateTime Inserted { get; protected set; }
-		public Int32 Number { get; protected set; }
-#else
 		public DateTime Inserted { get; private set; }
 		public Int32 Number { get; private set; }
-#endif
 
 		static EntityWithInsertTracking() {
 			Triggers<EntityWithInsertTracking>.Inserting += e => e.Entity.Inserted = DateTime.UtcNow;
@@ -21,11 +16,7 @@ namespace EntityFramework.Triggers.Tests {
 		}
 	}
 	public abstract class EntityWithTracking : EntityWithInsertTracking {
-#if EF_CORE
-		public DateTime Updated { get; protected set; }
-#else
 		public DateTime Updated { get; private set; }
-#endif
 
 		static EntityWithTracking() {
 			Triggers<EntityWithTracking>.Inserting += e => e.Entity.Updated = DateTime.UtcNow;
