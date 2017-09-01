8fb1160dacc99b842cd9c153e46f2be180aab49e
Nick Strupat <nick.strupat@gmail.com>
2017-08-31 22:54:21 -0400

    disable concurrent tests properly

--- TestsCore/TestBase.cs
+++ TestsCore/TestBase.cs
@@ -5,10 +5,16 @@ using Xunit;
 
 #if EF_CORE
 using Microsoft.EntityFrameworkCore;
-namespace EntityFrameworkCore.Triggers.Tests {
 #else
 using System.Data.Entity;
 using System.Data.Entity.Validation;
+#endif
+
+[assembly: CollectionBehavior(DisableTestParallelization = true)]
+
+#if EF_CORE
+namespace EntityFrameworkCore.Triggers.Tests {
+#else
 namespace EntityFramework.Triggers.Tests {
 #endif
 	public abstract class TestBase : IDisposable {
@@ -16,32 +22,25 @@ namespace EntityFramework.Triggers.Tests {
 		protected abstract void Teardown();
 
 		protected void DoATest(Action action) {
-			if (!semaphoreSlim.Wait(10000))
-				Assert.True(false, "Wait failed due to timeout");
 			Setup();
 			try {
 				action();
 			}
 			finally {
 				Teardown();
-				semaphoreSlim.Release();
 			}
 		}
 
 		protected async Task DoATestAsync(Func<Task> action) {
-			if (!await semaphoreSlim.WaitAsync(10000).ConfigureAwait(false))
-				Assert.True(false, "Wait failed due to timeout");
 			Setup();
 			try {
 				await action().ConfigureAwait(false);
 			}
 			finally {
 				Teardown();
-				semaphoreSlim.Release();
 			}
 		}
 
-		private static readonly SemaphoreSlim semaphoreSlim = new SemaphoreSlim(1, 1);
 		protected readonly Context Context = new Context();
 
 		public virtual void Dispose() => Context.Dispose();
