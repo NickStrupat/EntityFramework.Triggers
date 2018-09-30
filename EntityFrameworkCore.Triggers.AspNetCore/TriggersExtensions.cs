cbdffb60cb265519960b6d13fb0f2418269551ca
Nicholas Strupat <nick@Nicholass-MacBook-Pro.local>
2018-09-30 13:16:03 -0400

    add ASP.NET Core extensions

---
+++ EntityFrameworkCore.Triggers.AspNetCore/TriggersExtensions.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace EntityFrameworkCore.Triggers.AspNetCore
+{
+	public static class TriggersExtensions
+	{
+        public static IServiceCollection AddTriggers(this IServiceCollection serviceCollection)
+        {
+            return serviceCollection.AddSingleton(typeof(ITriggers<,>), typeof(Triggers<,>))
+                                    .AddSingleton(typeof(ITriggers<>), typeof(Triggers<>));
+        }
+
+        public static IApplicationBuilder UseTriggers(this IApplicationBuilder app, Action<ITriggersBuilder> configureTriggers)
+        {
+            Triggers.ServiceProvider = app.ApplicationServices;
+            configureTriggers?.Invoke(new TriggersBuilder(app.ApplicationServices));
+            return app;
+        }
+
+        private class TriggersBuilder : ITriggersBuilder
+        {
+            private readonly IServiceProvider serviceProvider;
+
+            public TriggersBuilder(IServiceProvider serviceProvider) => this.serviceProvider = serviceProvider;
+
+            public ITriggers<TEntity, TDbContext> Triggers<TEntity, TDbContext>()
+            where TEntity : class
+            where TDbContext : DbContext
+            {
+                return serviceProvider.GetService<ITriggers<TEntity, TDbContext>>();
+            }
+
+            public ITriggers<TEntity> Triggers<TEntity>()
+            where TEntity : class
+            {
+                return serviceProvider.GetService<ITriggers<TEntity>>();
+            }
+        }
+    }
+
+    public interface ITriggersBuilder {
+        ITriggers<TEntity, TDbContext> Triggers<TEntity, TDbContext>()
+        where TEntity : class
+        where TDbContext : DbContext;
+
+        ITriggers<TEntity> Triggers<TEntity>()
+        where TEntity : class;
+    }
+}
