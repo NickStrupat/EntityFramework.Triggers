fe7cecf4a2f18addcd879ff6ef7de470acf8fe0b
Nick Strupat <nick.strupat@gmail.com>
2018-10-11 00:48:52 -0400

    able to easily inject value tuples now; callers can now easily name their injected services via named value tuples

---
+++ src/EntityFrameworkCore.Triggers/ServiceRetrieval.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+#if EF_CORE
+namespace EntityFrameworkCore.Triggers
+#else
+namespace EntityFramework.Triggers
+#endif
+{
+	internal static class ServiceRetrieval
+	{
+		public static readonly Type[] ValueTupleTypes =
+		{
+			typeof(ValueTuple<>),
+			typeof(ValueTuple<,>),
+			typeof(ValueTuple<,,>),
+			typeof(ValueTuple<,,,>),
+			typeof(ValueTuple<,,,,>),
+			typeof(ValueTuple<,,,,,>),
+			typeof(ValueTuple<,,,,,,>),
+			typeof(ValueTuple<,,,,,,,>)
+		};
+	}
+}
---
+++ src/EntityFrameworkCore.Triggers/ServiceRetrieval_1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+using Microsoft.Extensions.DependencyInjection;
+
+#if EF_CORE
+namespace EntityFrameworkCore.Triggers
+#else
+namespace EntityFramework.Triggers
+#endif
+{
+	internal static class ServiceRetrieval<TService>
+	{
+		public static TService GetService(IServiceProvider serviceProvider) => func(serviceProvider);
+
+		private static readonly Func<IServiceProvider, TService> func =
+			typeof(TService).IsGenericType && IsAValueTupleType(typeof(TService), out var valueTupleFactory)
+				? valueTupleFactory
+				: ServiceProviderServiceExtensions.GetRequiredService<TService>;
+
+		private static Boolean IsAValueTupleType(Type type, out Func<IServiceProvider, TService> valueTupleFactory)
+		{
+			var genericTypeDefinition = type.GetGenericTypeDefinition();
+			if (ServiceRetrieval.ValueTupleTypes.Contains(genericTypeDefinition))
+				return GetValueTupleCreationDelegate(type.GetGenericArguments(), out valueTupleFactory);
+			valueTupleFactory = null;
+			return false;
+		}
+
+		private static Boolean GetValueTupleCreationDelegate(Type[] genericTypes, out Func<IServiceProvider, TService> valueTupleFactory)
+		{
+			var create = typeof(ValueTuple).GetMethods().Single(IsValueTupleCreateMethod).MakeGenericMethod(genericTypes);
+			var parameter = Expression.Parameter(typeof(IServiceProvider));
+			var arguments = genericTypes.Select(genericType => Expression.Convert(GetCall(sp => sp.GetRequiredService(genericType)), genericType));
+			var call = Expression.Call(create, arguments);
+			valueTupleFactory = Expression.Lambda<Func<IServiceProvider, TService>>(call, parameter).Compile();
+			return true;
+			
+			Boolean IsValueTupleCreateMethod(MethodInfo x) => x.Name == nameof(ValueTuple.Create) && x.IsGenericMethod && x.GetGenericArguments().Length == genericTypes.Length;
+			MethodCallExpression GetCall(Func<IServiceProvider, Object> serviceGetter) => Expression.Call(Expression.Constant(serviceGetter.Target), serviceGetter.Method, parameter);
+		}
+	}
+}
