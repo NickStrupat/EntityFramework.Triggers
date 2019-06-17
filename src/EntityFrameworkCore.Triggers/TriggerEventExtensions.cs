80d49461636179e2fcaed1fe1f90fd3d4683671b
NickStrupat <nick.strupat@gmail.com>
2019-06-16 23:39:50 -0400

    add async raise extension method

--- src/EntityFrameworkCore.Triggers/TriggerEventExtensions.cs
+++ src/EntityFrameworkCore.Triggers/TriggerEventExtensions.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Threading.Tasks;
 
 #if EF_CORE
 namespace EntityFrameworkCore.Triggers
@@ -9,5 +10,6 @@ namespace EntityFramework.Triggers
 	internal static class TriggerEventExtensions
 	{
 		public static void Raise(this ITriggerEvent triggersEvent, Object entry) => ((ITriggerEventInternal)triggersEvent).Raise(entry);
+		public static Task RaiseAsync(this ITriggerEvent triggersEvent, Object entry) => ((ITriggerEventInternal)triggersEvent).RaiseAsync(entry);
 	}
 }
