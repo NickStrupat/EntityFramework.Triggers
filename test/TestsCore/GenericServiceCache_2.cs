83f6e1336f9e18eaef22dcbb2e6944aa9ced1ed1
NickStrupat <nick.strupat@gmail.com>
2019-06-16 23:47:12 -0400

    fix namespace for the different versions

--- test/TestsCore/GenericServiceCache_2.cs
+++ test/TestsCore/GenericServiceCache_2.cs
@@ -2,8 +2,14 @@
 using Xunit;
 
 #if EF_CORE
+# if NETCOREAPP2_0
 namespace EntityFrameworkCore.Triggers.Tests
+# else
+namespace EntityFrameworkCore.Triggers.Tests.Net461
+# endif
 #else
+using System.Data.Entity;
+using System.Data.Entity.Validation;
 namespace EntityFramework.Triggers.Tests
 #endif
 {
