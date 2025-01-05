6011e65fc8b3075d3626b94300122a867bbb09ea
NickStrupat <nick.strupat@gmail.com>
2025-01-04 22:49:09 -0500

    remove the Hosting and AspNetCore projects in favour of using extending IServiceProvider itself

---
+++ src/EntityFrameworkCore.Triggers/ServiceProviderExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+#if EF_CORE
+namespace EntityFrameworkCore.Triggers
+#else
+namespace EntityFramework.Triggers
+#endif
+{
+	public static class ServiceProviderExtensions
+	{
+		public static IServiceProvider UseTriggers(this IServiceProvider services, Action<ITriggersBuilder> configureTriggers)
+		{
+			if (services == null)
+				throw new ArgumentNullException(nameof(services));
+			if (configureTriggers == null)
+				throw new ArgumentNullException(nameof(configureTriggers));
+			var triggersBuilder = TriggersBuilderFactory.Create(services);
+			configureTriggers.Invoke(triggersBuilder);
+			return services;
+		}
+	}
+}
